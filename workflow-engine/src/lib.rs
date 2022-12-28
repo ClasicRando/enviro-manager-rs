add2aedb58c74241af8234aad4964d4fa455ae53
Steven Thomson <steventhomson9@gmail.com>
2022-12-27 23:16:50 -0500

    feat: add db connection utility

--- workflow-engine/src/lib.rs
+++ workflow-engine/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use sqlx::{postgres::{PgConnectOptions, PgPoolOptions}, PgPool};
+
+fn we_db_options() -> Result<PgConnectOptions, sqlx::Error> {
+    let we_host_address = env!("WE_HOST");
+    let we_db_name = env!("WE_DB");
+    let we_db_user = env!("WE_USER");
+    let we_db_password = env!("WE_PASSWORD");
+    let options = PgConnectOptions::new()
+        .host(we_host_address)
+        .database(we_db_name)
+        .options([("search_path", "workflow_engine")])
+        .username(we_db_user)
+        .password(we_db_password);
+    Ok(options)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub async fn create_we_db_pool() -> Result<PgPool, sqlx::Error> {
+    let options = we_db_options()?;
+    let pool = PgPoolOptions::new()
+        .min_connections(10)
+        .max_connections(20)
+        .connect_with(options)
+        .await?;
+    Ok(pool)
 }
