14323c5fa1ef367f71fe1ed405e1e1586513da51
Steven Thomson <steventhomson9@gmail.com>
2023-01-02 22:32:30 -0500

    feat: add executor binary

---
+++ workflow-engine/src/bin/workflow_engine_executor.rs
@@ -0,0 +1,29 @@
+use log::{error, info};
+use workflow_engine::{
+    executors_service, task_queue_service, workflow_runs_service, Executor, Result as WEResult,
+};
+
+#[tokio::main]
+async fn main() -> WEResult<()> {
+    log4rs::init_file("workflow-engine/executor_log.yml", Default::default()).unwrap();
+
+    info!("Initializing Executor");
+    let executor_service = executors_service().await?;
+    let wr_service = workflow_runs_service().await?;
+    let tq_service = task_queue_service().await?;
+    let mut executor = match Executor::new(executor_service, wr_service, tq_service).await {
+        Ok(executor) => executor,
+        Err(error) => {
+            error!("{}", error);
+            return Ok(());
+        }
+    };
+    let executor_id = executor.executor_id;
+
+    info!("Running Executor, id = {}", executor_id);
+    if let Err(error) = executor.run().await {
+        executor_service.post_error(executor_id, error).await?;
+    }
+    info!("Exiting executor, id = {}", executor_id);
+    Ok(())
+}
