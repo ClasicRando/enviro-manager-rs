9bce088c88504f57839f6c16968d4d3c91d73364
Steven Thomson <steventhomson9@gmail.com>
2023-01-02 22:01:04 -0500

    feat: consolidate transaction error

--- workflow-engine/src/database/mod.rs
+++ workflow-engine/src/database/mod.rs
@@ -1,3 +1,3 @@
 mod utilities;
 
-pub use utilities::{create_db_pool, finish_transaction, we_db_pool, TransactionError};
+pub use utilities::{create_db_pool, finish_transaction, we_db_pool};
--- workflow-engine/src/database/utilities.rs
+++ workflow-engine/src/database/utilities.rs
@@ -1,9 +1,12 @@
+use std::env;
+
 use async_once_cell::OnceCell;
 use sqlx::{
-    postgres::{PgConnectOptions, PgPool, PgPoolOptions},
-    Postgres, Transaction,
+    postgres::{PgConnectOptions, PgPoolOptions},
+    PgPool, Postgres, Transaction,
 };
-use std::{env, fmt::Display};
+
+use crate::error::{Error as WEError, Result as WEResult};
 
 static WE_POSTGRES_DB: OnceCell<PgPool> = OnceCell::new();
 
@@ -31,41 +34,20 @@ pub async fn we_db_pool() -> Result<&'static PgPool, sqlx::Error> {
     WE_POSTGRES_DB.get_or_try_init(create_db_pool()).await
 }
 
-#[derive(Debug)]
-pub enum TransactionError {
-    Sql(sqlx::Error),
-    CommitError(sqlx::Error),
-    RollbackError { orig: sqlx::Error, new: sqlx::Error },
-}
-
-impl Display for TransactionError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            TransactionError::Sql(e) => write!(f, "{}", e),
-            TransactionError::CommitError(e) => write!(f, "Error during transaction commit: {}", e),
-            TransactionError::RollbackError { orig, new } => write!(
-                f,
-                "Error during transaction rollback: {}\nOriginal Error: {}",
-                new, orig
-            ),
-        }
-    }
-}
-
 pub async fn finish_transaction<T>(
     transaction: Transaction<'_, Postgres>,
     result: Result<T, sqlx::Error>,
-) -> Result<T, TransactionError> {
+) -> WEResult<T> {
     match result {
         Ok(inner) => {
             if let Err(error) = transaction.commit().await {
-                return Err(TransactionError::CommitError(error));
+                return Err(WEError::CommitError(error));
             }
             Ok(inner)
         }
         Err(error) => match transaction.rollback().await {
-            Ok(_) => Err(TransactionError::Sql(error)),
-            Err(t_error) => Err(TransactionError::RollbackError {
+            Ok(_) => Err(WEError::Sql(error)),
+            Err(t_error) => Err(WEError::RollbackError {
                 orig: error,
                 new: t_error,
             }),
