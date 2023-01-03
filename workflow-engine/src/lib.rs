31292c9a7673fe5e5dff6f173858a977e5bd234c
Steven Thomson <steventhomson9@gmail.com>
2023-01-02 22:24:19 -0500

    feat: add lib exports

--- workflow-engine/src/lib.rs
+++ workflow-engine/src/lib.rs
@@ -1,6 +1,10 @@
+pub mod database;
 mod error;
 mod executor;
 mod services;
-pub mod database;
 
-pub use services::task_queue::TaskResponse;
+pub use error::{Error, Result};
+pub use executor::Executor;
+pub use services::{
+    executors_service, task_queue::TaskResponse, task_queue_service, workflow_runs_service,
+};
