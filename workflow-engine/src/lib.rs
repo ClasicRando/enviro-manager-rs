3c07f5915cfd371f828ab1727d40955c08fa8a41
Steven Thomson <steventhomson9@gmail.com>
2023-01-03 23:44:06 -0500

    feat: add job worker module and job service

--- workflow-engine/src/lib.rs
+++ workflow-engine/src/lib.rs
@@ -1,10 +1,13 @@
 pub mod database;
 mod error;
 mod executor;
+mod job_worker;
 mod services;
 
 pub use error::{Error, Result};
 pub use executor::Executor;
+pub use job_worker::JobWorker;
 pub use services::{
-    executors_service, task_queue::TaskResponse, task_queue_service, workflow_runs_service,
+    executors_service, jobs_service, task_queue::TaskResponse, task_queue_service,
+    workflow_runs_service,
 };
