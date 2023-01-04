36f43165157e6ed9d920aa1bbf5cd1e1faca9e70
Steven Thomson <steventhomson9@gmail.com>
2023-01-03 23:44:24 -0500

    feat: add job worker bin

---
+++ workflow-engine/src/bin/job_worker.rs
@@ -0,0 +1,25 @@
+use log::{error, info};
+use workflow_engine::{jobs_service, JobWorker, Result as WEResult};
+
+#[tokio::main]
+async fn main() -> WEResult<()> {
+    log4rs::init_file("job_worker_log4rs.yml", Default::default()).unwrap();
+    
+    info!("Initializing Worker");
+    let jobs_service = jobs_service().await?;
+    let mut worker = match JobWorker::new(jobs_service).await {
+        Ok(worker) => worker,
+        Err(error) => {
+            error!("{}", error);
+            return Err(error);
+        }
+    };
+
+    info!("Running Worker");
+    if let Err(error) = worker.run().await {
+        error!("Error during worker run\n{}", error)
+    }
+
+    info!("Exiting Worker");
+    Ok(())
+}
