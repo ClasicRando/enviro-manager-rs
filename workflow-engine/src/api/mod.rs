5a87b97625de0eff7dd0a6d8c8b58f3b0d55adad
Steven Thomson <steventhomson9@gmail.com>
2023-01-05 21:16:57 -0500

    feat: add services to rocket build

--- workflow-engine/src/api/mod.rs
+++ workflow-engine/src/api/mod.rs
@@ -1,15 +1,28 @@
 use rocket::{routes, Build, Config, Rocket};
 
-use crate::{database::create_db_pool, Result as WEResult};
+use crate::{
+    executors_service, jobs_service, task_queue_service, tasks_service, workflow_runs_service,
+    workflows_service, Result as WEResult,
+};
 
 pub async fn build_api() -> WEResult<Rocket<Build>> {
-    let pool = create_db_pool().await?;
+    let executors_service = executors_service().await?;
+    let jobs_service = jobs_service().await?;
+    let task_queue_service = task_queue_service().await?;
+    let tasks_service = tasks_service().await?;
+    let workflow_runs_service = workflow_runs_service().await?;
+    let workflows_service = workflows_service().await?;
     let config = Config {
         port: 8000,
         ..Default::default()
     };
     let build = rocket::build()
-        .manage(pool)
+        .manage(executors_service)
+        .manage(jobs_service)
+        .manage(task_queue_service)
+        .manage(tasks_service)
+        .manage(workflow_runs_service)
+        .manage(workflows_service)
         .configure(config)
         .mount("/api/v1/", routes![]);
     Ok(build)
