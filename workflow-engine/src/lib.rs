a426c308972d717802a9b62b22db6f2bb9998c31
Steven Thomson <steventhomson9@gmail.com>
2023-01-05 23:00:10 -0500

    feat: export create service function for api

--- workflow-engine/src/lib.rs
+++ workflow-engine/src/lib.rs
@@ -9,6 +9,10 @@ pub use api::build_api;
 pub use error::{Error, Result};
 pub use executor::Executor;
 pub use job_worker::JobWorker;
+pub(crate) use services::{
+    create_executors_service, create_jobs_service, create_task_queue_service, create_tasks_service,
+    create_workflow_runs_service, create_workflows_service,
+};
 pub use services::{
     executors_service, jobs_service, task_queue::TaskResponse, task_queue_service, tasks_service,
     workflow_runs_service, workflows_service,
