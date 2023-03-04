4ca8b2f2c9551b47da2c52ce36ef8c54d880bef2
Steven Thomson <steventhomson9@gmail.com>
2023-03-04 16:14:30 -0500

    feat: move enum check into generated statements

--- common/src/db_test.rs
+++ common/src/db_test.rs
@@ -1,3 +1,5 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use sqlx::PgPool;
 use std::path::PathBuf;
 use tokio::fs::read_dir;
@@ -34,12 +36,53 @@ async fn run_common_db_tests(
     run_tests(tests, pool).await
 }
 
+lazy_static! {
+    static ref ENUM_REGEX: Regex = Regex::new(
+        r"^create type (?P<schema>[^.]+)\.(?P<name>[^.]+) as enum\s*\((?P<labels>[^;]+)\s*\);"
+    )
+    .unwrap();
+}
+
+async fn check_enum(block: String, pool: &PgPool) -> Result<(), Box<dyn std::error::Error>> {
+    let Some(captures) = ENUM_REGEX.captures(&block) else {
+        Err(format!("Block was a not a match to the ENUM_REGEX"))?
+    };
+    let Some(schema) = captures.name("schema") else {
+        Err(format!("No 'schema' capture group present in enum definition"))?
+    };
+    let Some(name) = captures.name("name") else {
+        Err(format!("No 'name' capture group present in enum definition"))?
+    };
+    let Some(labels) = captures.name("labels") else {
+        Err(format!("No 'labels' capture group present in enum definition"))?
+    };
+    let labels: Vec<&str> = labels
+        .as_str()
+        .split(',')
+        .map(|label| label.trim().trim_matches('\''))
+        .collect();
+    sqlx::query("call data_check.check_enum_definition($1,$2,$3)")
+        .bind(schema.as_str())
+        .bind(name.as_str())
+        .bind(labels)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
 pub async fn run_db_tests(pool: PgPool) -> Result<(), Box<dyn std::error::Error>> {
     let schema_directory = package_dir().join("database");
     let build_file = schema_directory.join("build.json");
     let db_build = db_build(build_file).await?;
     for common_schema in &db_build.common_dependencies {
         run_common_db_tests(&pool, common_schema).await?;
+    }
+
+    for entry in db_build.entries {
+        let block = read_file(schema_directory.join(&entry.name)).await?;
+        if ENUM_REGEX.is_match(&block) {
+            check_enum(block, &pool).await?
+        }
     }
 
     let tests = schema_directory.join("tests");
--- workflow-engine/database/tests/enum_definitions.pgsql
+++
@@ -1,8 +0,0 @@
-declare
-    v_schema text := 'workflow_engine';
-begin
-    call data_check.check_enum_definition(v_schema,'executor_status','{Active,Canceled,Shutdown}');
-    call data_check.check_enum_definition(v_schema,'job_type','{Scheduled,Interval}');
-    call data_check.check_enum_definition(v_schema,'task_status','{Waiting,Running,Paused,Failed,Rule Broken,Complete,Canceled}');
-    call data_check.check_enum_definition(v_schema,'workflow_run_status','{Waiting,Scheduled,Running,Paused,Failed,Complete,Canceled}');
-end;
