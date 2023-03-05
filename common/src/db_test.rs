8dc43e65140e974de5243fab743f8c3e40044055
Steven Thomson <steventhomson9@gmail.com>
2023-03-05 18:39:57 -0500

    feat: utilize composite check procedure

--- common/src/db_test.rs
+++ common/src/db_test.rs
@@ -38,13 +38,17 @@ async fn run_common_db_tests(
 
 lazy_static! {
     static ref ENUM_REGEX: Regex = Regex::new(
-        r"^create type (?P<schema>[^.]+)\.(?P<name>[^.]+) as enum\s*\((?P<labels>[^;]+)\s*\);"
+        r"^create\s+type\s+(?P<schema>[^.]+)\.(?P<name>[^.]+)\s+as\s+enum\s*\((?P<labels>[^;]+)\s*\);"
+    )
+    .unwrap();
+    static ref COMPOSITE_REGEX: Regex = Regex::new(
+        r"^create\s+type\s+(?P<schema>[^.]+)\.(?P<name>[^.]+?)\s+as\s*\((?P<attributes>[^;]+)\);"
     )
     .unwrap();
 }
 
-async fn check_for_enum(block: String, pool: &PgPool) -> Result<(), Box<dyn std::error::Error>> {
-    let Some(captures) = ENUM_REGEX.captures(&block) else {
+async fn check_for_enum(block: &str, pool: &PgPool) -> Result<(), Box<dyn std::error::Error>> {
+    let Some(captures) = ENUM_REGEX.captures(block) else {
         return Ok(())
     };
     let Some(schema) = captures.name("schema") else {
@@ -70,6 +74,33 @@ async fn check_for_enum(block: String, pool: &PgPool) -> Result<(), Box<dyn std:
     Ok(())
 }
 
+async fn check_for_composite(block: &str, pool: &PgPool) -> Result<(), Box<dyn std::error::Error>> {
+    let Some(captures) = COMPOSITE_REGEX.captures(block) else {
+        return Ok(())
+    };
+    let Some(schema) = captures.name("schema") else {
+        Err(format!("No 'schema' capture group present in composite definition"))?
+    };
+    let Some(name) = captures.name("name") else {
+        Err(format!("No 'name' capture group present in composite definition"))?
+    };
+    let Some(attributes) = captures.name("attributes") else {
+        Err(format!("No 'attributes' capture group present in composite definition"))?
+    };
+    let attributes: Vec<&str> = attributes
+        .as_str()
+        .split(',')
+        .map(|label| label.trim())
+        .collect();
+    sqlx::query("call data_check.check_composite_definition($1,$2,$3)")
+        .bind(schema.as_str())
+        .bind(name.as_str())
+        .bind(attributes)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
 pub async fn run_db_tests(pool: PgPool) -> Result<(), Box<dyn std::error::Error>> {
     let schema_directory = package_dir().join("database");
     let build_file = schema_directory.join("build.json");
@@ -80,7 +111,8 @@ pub async fn run_db_tests(pool: PgPool) -> Result<(), Box<dyn std::error::Error>
 
     for entry in db_build.entries {
         let block = read_file(schema_directory.join(&entry.name)).await?;
-        check_for_enum(block, &pool).await?
+        check_for_enum(&block, &pool).await?;
+        check_for_composite(&block, &pool).await?;
     }
 
     let tests = schema_directory.join("tests");
