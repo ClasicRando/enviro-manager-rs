5253f11f1699c23167c826d6003f2f67371445bb
Steven Thomson <steventhomson9@gmail.com>
2023-03-11 12:56:48 -0500

    feat: add test db connection pool creation

--- workflow-engine/src/database/mod.rs
+++ workflow-engine/src/database/mod.rs
@@ -1,3 +1,4 @@
 mod utilities;
 
-pub use utilities::{create_db_pool, we_db_pool};
+pub use utilities::{create_db_pool, we_db_pool, create_test_db_pool};
+
--- workflow-engine/src/database/utilities.rs
+++ workflow-engine/src/database/utilities.rs
@@ -23,12 +23,35 @@ fn db_options() -> WEResult<PgConnectOptions> {
     Ok(options)
 }
 
+/// Return test database connect options
+fn test_db_options() -> WEResult<PgConnectOptions> {
+    let port = env::var("WE_TEST_PORT")?
+        .parse()
+        .expect("Port environment variable is not an integer");
+    let options = PgConnectOptions::new()
+        .host(&env::var("WE_TEST_HOST")?)
+        .port(port)
+        .database(&env::var("WE_TEST_DB")?)
+        .username(&env::var("WE_TEST_USER")?)
+        .password(&env::var("WE_TEST_PASSWORD")?);
+    Ok(options)
+}
+
 /// Return a new pool of postgres connections
 pub async fn create_db_pool() -> WEResult<PgPool> {
     let options = db_options()?;
     let pool = PgPoolOptions::new()
         .min_connections(10)
         .max_connections(20)
+        .connect_with(options)
+        .await?;
+    Ok(pool)
+}
+
+/// Return a new pool of postgres connections for the test database
+pub async fn create_test_db_pool() -> WEResult<PgPool> {
+    let options = test_db_options()?;
+    let pool = PgPoolOptions::new()
         .connect_with(options)
         .await?;
     Ok(pool)
