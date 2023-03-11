d90dbe79634d3d0843d769b037890ad0c52bee6a
Steven Thomson <steventhomson9@gmail.com>
2023-03-11 12:57:08 -0500

    feat: use test database connection

--- workflow-engine/tests/database_tests.rs
+++ workflow-engine/tests/database_tests.rs
@@ -1,9 +1,9 @@
 use common::db_test::run_db_tests;
-use workflow_engine::database::create_db_pool;
+use workflow_engine::database::create_test_db_pool;
 
 #[tokio::test]
 async fn run_workflow_engine_database_tests() -> Result<(), Box<dyn std::error::Error>> {
-    let pool = create_db_pool().await?;
+    let pool = create_test_db_pool().await?;
     run_db_tests(pool).await?;
     Ok(())
 }
