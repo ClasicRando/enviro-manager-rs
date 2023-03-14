7bde26d1c04e0c6a2c91a10b62cf9de7d6f554ba
Steven Thomson <steventhomson9@gmail.com>
2023-03-13 22:24:22 -0400

    feat: remove test db build block

--- workflow-engine/src/bin/we_db_build.rs
+++ workflow-engine/src/bin/we_db_build.rs
@@ -4,12 +4,5 @@ use common::db_build::build_schema;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pool = workflow_engine::database::create_db_pool().await?;
     build_schema(&pool).await?;
-    if let Ok(db) = std::env::var("WE_TEST_DB") {
-        if db.trim().is_empty() {
-            return Ok(())
-        }
-        let pool = workflow_engine::database::create_test_db_pool().await?;
-        build_schema(&pool).await?;
-    }
     Ok(())
 }
