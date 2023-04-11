0772f0095f65e5625259e54e19fad8cecd091cdb
Steven Thomson <steventhomson9@gmail.com>
2023-04-10 21:38:07 -0400

    chore: move database tests into database module

--- workflow-engine/src/database/mod.rs
+++ workflow-engine/src/database/mod.rs
@@ -1,4 +1,39 @@
 mod utilities;
 
-pub use utilities::{create_db_pool, we_db_pool, create_test_db_pool};
+pub use utilities::{create_db_pool, we_db_pool, create_test_db_pool, we_test_db_pool};
 
+#[cfg(test)]
+mod test {
+    use common::db_test::run_db_tests;
+    use crate::database::{create_db_pool, create_test_db_pool};
+
+    async fn refresh_test_database() -> Result<(), Box<dyn std::error::Error>> {
+        let pool = create_db_pool().await?;
+        sqlx::query("drop database if exists workflow_engine_test")
+            .execute(&pool)
+            .await?;
+        sqlx::query(
+            r#"
+            create database workflow_engine_test with
+                owner = workflow_engine_admin
+                encoding = 'UTF8'"#,
+        )
+        .execute(&pool)
+        .await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn run_workflow_engine_database_tests() -> Result<(), Box<dyn std::error::Error>> {
+        let db_refresh = std::env::var("WE_TEST_REFRESH")
+            .map(|v| &v == "true")
+            .unwrap_or_default();
+        if db_refresh {
+            refresh_test_database().await?;
+        }
+
+        let pool = create_test_db_pool().await?;
+        run_db_tests(pool).await?;
+        Ok(())
+    }
+}
