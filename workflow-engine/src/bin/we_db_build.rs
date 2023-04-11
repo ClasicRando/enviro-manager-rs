51eaeefdebfc6213c4bbab5c10fe48b26dcb8e0a
Steven Thomson <steventhomson9@gmail.com>
2023-04-10 22:19:46 -0400

    feat: move test build and refresh to build bin

--- workflow-engine/src/bin/we_db_build.rs
+++ workflow-engine/src/bin/we_db_build.rs
@@ -1,8 +1,51 @@
 use common::db_build::build_database;
+use workflow_engine::database::{we_db_pool, we_test_db_pool};
+
+///
+async fn refresh_test_database() -> Result<(), Box<dyn std::error::Error>> {
+    let pool = we_db_pool().await?;
+    sqlx::query("drop database if exists workflow_engine_test")
+        .execute(pool)
+        .await?;
+    sqlx::query(
+        r#"
+        create database workflow_engine_test with
+            owner = workflow_engine_admin
+            encoding = 'UTF8'"#,
+    )
+    .execute(pool)
+    .await?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pool = workflow_engine::database::we_db_pool().await?;
+    let db_refresh = std::env::var("WE_TEST_REFRESH")
+        .map(|v| &v == "true")
+        .unwrap_or_default();
+
+    let pool = match std::env::var("WE_DB_BUILD_TARGET").ok() {
+        Some(name) if name == "test" => {
+            println!("Target specified as 'test' to rebuild");
+            if db_refresh {
+                println!("Refresh specified for the test database");
+                refresh_test_database().await?;
+            }
+            we_test_db_pool().await?
+        }
+        Some(name) if name == "prod" => {
+            println!("Target specified as 'prod' to rebuild");
+            we_db_pool().await?
+        }
+        Some(name) => {
+            println!("Target specified in 'WE_DB_BUILD_TARGET' environment variable ('{}') was not valid. Acceptable values are 'test' or 'prod'", name);
+            return Ok(());
+        }
+        None => {
+            println!("Could not find a value for the database build target. Please specify with the 'WE_DB_BUILD_TARGET' environment variable");
+            return Ok(());
+        }
+    };
     build_database(pool).await?;
     Ok(())
 }
--- workflow-engine/src/database/mod.rs
+++ workflow-engine/src/database/mod.rs
@@ -5,33 +5,10 @@ pub use utilities::{we_db_pool, we_test_db_pool};
 #[cfg(test)]
 mod test {
     use common::db_test::run_db_tests;
-    use super::{we_db_pool, we_test_db_pool};
-
-    async fn refresh_test_database() -> Result<(), Box<dyn std::error::Error>> {
-        let pool = we_db_pool().await?;
-        sqlx::query("drop database if exists workflow_engine_test")
-            .execute(pool)
-            .await?;
-        sqlx::query(
-            r#"
-            create database workflow_engine_test with
-                owner = workflow_engine_admin
-                encoding = 'UTF8'"#,
-        )
-        .execute(pool)
-        .await?;
-        Ok(())
-    }
+    use super::we_test_db_pool;
 
     #[tokio::test]
     async fn run_workflow_engine_database_tests() -> Result<(), Box<dyn std::error::Error>> {
-        let db_refresh = std::env::var("WE_TEST_REFRESH")
-            .map(|v| &v == "true")
-            .unwrap_or_default();
-        if db_refresh {
-            refresh_test_database().await?;
-        }
-
         let pool = we_test_db_pool().await?;
         run_db_tests(pool).await?;
         Ok(())
