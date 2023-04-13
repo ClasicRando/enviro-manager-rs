4f448c15b8bd8ce7e3f839c85410992b85205252
Steven Thomson <steventhomson9@gmail.com>
2023-04-13 17:43:25 -0400

    feat: revert back to pool creation

--- workflow-engine/src/database/mod.rs
+++ workflow-engine/src/database/mod.rs
@@ -5,12 +5,12 @@ pub use utilities::{we_db_pool, we_test_db_pool};
 #[cfg(test)]
 mod test {
     use common::db_test::run_db_tests;
-    use super::we_test_db_pool;
+    use crate::database::utilities::create_test_db_pool;
 
     #[tokio::test]
     async fn run_workflow_engine_database_tests() -> Result<(), Box<dyn std::error::Error>> {
-        let pool = we_test_db_pool().await?;
-        run_db_tests(pool).await?;
+        let pool = create_test_db_pool().await?;
+        run_db_tests(&pool).await?;
         Ok(())
     }
 }
