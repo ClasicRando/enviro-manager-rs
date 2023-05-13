2b073bc4d426fe0bb8e8fe8af779008df9ae5195
Steven Thomson <steventhomson9@gmail.com>
2023-05-13 18:49:33 -0400

    chore: remove unused service creation methods

--- users/src/service/mod.rs
+++ users/src/service/mod.rs
@@ -1,26 +1,3 @@
-use common::error::EmResult;
-use sqlx::{Database, Pool};
-
-use self::{roles::RoleService, users::UserService};
-
 pub mod roles;
 pub mod users;
 pub mod postgres;
-
-///
-pub fn create_roles_service<R, D>(pool: &Pool<D>) -> EmResult<R>
-where
-    R: RoleService<Database = D>,
-    D: Database,
-{
-    Ok(R::new(pool))
-}
-
-///
-pub fn create_users_service<U, D>(pool: &Pool<D>) -> EmResult<U>
-where
-    U: UserService<Database = D>,
-    D: Database,
-{
-    Ok(U::new(pool))
-}
