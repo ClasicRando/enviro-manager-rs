a82ca907280149f7d9e2ec79465e054210237e38
Steven Thomson <steventhomson9@gmail.com>
2023-05-16 22:53:24 -0400

    feat: remove database test runner

--- users/src/database/mod.rs
+++ users/src/database/mod.rs
@@ -44,20 +44,3 @@ pub fn test_db_options() -> EmResult<PgConnectOptions> {
         .password(&env::var("USERS_TEST_PASSWORD")?);
     Ok(options)
 }
-
-#[cfg(test)]
-mod test {
-    use common::{
-        database::{ConnectionBuilder, PgConnectionBuilder},
-        db_test::run_db_tests,
-    };
-
-    use super::test_db_options;
-
-    #[tokio::test]
-    async fn run_workflow_engine_database_tests() -> Result<(), Box<dyn std::error::Error>> {
-        let pool = PgConnectionBuilder::create_pool(test_db_options()?, 1, 1).await?;
-        run_db_tests(&pool).await?;
-        Ok(())
-    }
-}
