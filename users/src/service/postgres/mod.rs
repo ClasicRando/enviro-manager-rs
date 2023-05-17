62445ac1d17c45880b2d6a96bfb44b1d049e1c8f
Steven Thomson <steventhomson9@gmail.com>
2023-05-16 22:54:03 -0400

    feat: create database test for test scripts

--- users/src/service/postgres/mod.rs
+++ users/src/service/postgres/mod.rs
@@ -3,8 +3,12 @@ pub mod users;
 
 #[cfg(test)]
 mod test {
-    use common::database::{ConnectionBuilder, PgConnectionBuilder};
-    use rstest::fixture;
+    use common::{
+        database::{ConnectionBuilder, PgConnectionBuilder},
+        error::{EmError, EmResult},
+        execute_anonymous_block, package_dir, read_file,
+    };
+    use rstest::{fixture, rstest};
     use sqlx::PgPool;
 
     use crate::database::test_db_options;
@@ -14,5 +18,18 @@ mod test {
     pub(crate) fn database() -> PgPool {
         let options = test_db_options().expect("Failed to create test database options");
         PgConnectionBuilder::create_pool_lazy(options, 20, 0)
+    }
+
+    #[rstest]
+    #[case("users/check_user_role.pgsql")]
+    #[case("users/validate_password.pgsql")]
+    #[tokio::test]
+    async fn database_test(database: &PgPool, #[case] test_path: &str) -> EmResult<()> {
+        let path = package_dir().join("database/tests").join(test_path);
+        let Ok(block) = read_file(&path).await else {
+            return Err(EmError::Generic(format!("Could not open file '{:?}'", path)))
+        };
+        execute_anonymous_block(&block, database).await?;
+        Ok(())
     }
 }
