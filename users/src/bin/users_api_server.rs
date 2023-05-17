1da7e737c69c4f66f5944b004cc3fbb0ea8506ef
Steven Thomson <steventhomson9@gmail.com>
2023-05-16 21:52:11 -0400

    feat: move to web user for api, update log file path

--- users/src/bin/users_api_server.rs
+++ users/src/bin/users_api_server.rs
@@ -2,16 +2,16 @@ use common::{database::PgConnectionBuilder, error::EmResult};
 use sqlx::Postgres;
 use users::{
     api,
-    database::db_options,
+    database::db_web_options,
     service::postgres::{roles::PgRoleService, users::PgUserService},
 };
 
 #[tokio::main]
 async fn main() -> EmResult<()> {
-    log4rs::init_file("users/api_server_log.yml", Default::default()).unwrap();
+    log4rs::init_file("users/users_api_server_log.yml", Default::default()).unwrap();
     api::spawn_api_server::<(&str, u16), PgConnectionBuilder, Postgres, PgRoleService, PgUserService>(
         ("127.0.0.1", 8080),
-        db_options()?,
+        db_web_options()?,
     )
     .await?;
     Ok(())
