a1e3445702951a8c4c9d9c568f88f261e34de689
Steven Thomson <steventhomson9@gmail.com>
2023-05-26 09:32:01 -0400

    feat: update to redis actor session store

--- users/src/api/mod.rs
+++ users/src/api/mod.rs
@@ -1,6 +1,6 @@
 use std::net::ToSocketAddrs;
 
-use actix_session::{storage::SessionStore, SessionMiddleware};
+use actix_session::{storage::RedisActorSessionStore, SessionMiddleware};
 use actix_web::{
     cookie::{Key, SameSite},
     middleware::Logger,
@@ -19,10 +19,9 @@ use crate::service::{roles::RoleService, users::UserService};
 /// you must have created a [ConnectionBuilder], [RoleService] and [UserService] for your desired
 /// [Database] implementation. Each component depends of a [Database] type so the system cannot
 /// contain disjointed service implementations to operate.
-pub async fn spawn_api_server<A, C, D, R, S, U>(
+pub async fn spawn_api_server<A, C, D, R, U>(
     address: A,
     options: <D::Connection as Connection>::Options,
-    session_store: S,
     signing_key: Key,
 ) -> EmResult<()>
 where
@@ -30,28 +29,31 @@ where
     C: ConnectionBuilder<D>,
     D: Database,
     R: RoleService<UserService = U> + Send + Sync + 'static,
-    S: SessionStore + Send + Clone,
     U: UserService<Database = D> + Send + Sync + 'static,
 {
     let pool = C::create_pool(options, 20, 10).await?;
     let users_service: Data<U> = Data::new(U::create(&pool));
     let roles_service: Data<R> = Data::new(R::create(users_service.get_ref()));
+    let redis_connection_string = std::env::var("REDIS_CONNECTION")?;
     HttpServer::new(move || {
         App::new().service(
             actix_web::web::scope("/api/v1")
                 .wrap(Logger::default())
                 .wrap(
-                    SessionMiddleware::builder(session_store.clone(), signing_key.clone())
-                        .cookie_http_only(false)
-                        .cookie_same_site(SameSite::Strict)
-                        .build(),
+                    SessionMiddleware::builder(
+                        RedisActorSessionStore::new(&redis_connection_string),
+                        signing_key.clone(),
+                    )
+                    .cookie_http_only(false)
+                    .cookie_same_site(SameSite::Strict)
+                    .build(),
                 )
                 .app_data(roles_service.clone())
                 .app_data(users_service.clone())
                 .route("/roles", get().to(roles::roles::<R>))
                 .route("/users", post().to(users::create_user::<U>))
-                .route("/users/username", patch().to(users::update_user::<U>))
-                .route("/users/validate", patch().to(users::validate_user::<U>))
+                .route("/users", patch().to(users::update_user::<U>))
+                .route("/users/validate", post().to(users::validate_user::<U>))
                 .route("/users/role", post().to(users::modify_user_role::<U>)),
         )
     })
--- users/src/bin/users_api_server.rs
+++ users/src/bin/users_api_server.rs
@@ -1,8 +1,6 @@
-use actix_session::storage::RedisSessionStore;
 use actix_web::cookie::Key;
 use common::{database::connection::PgConnectionBuilder, error::EmResult};
 use sqlx::Postgres;
-use common::error::EmError;
 use users::{
     api,
     database::db_web_options,
@@ -12,19 +10,14 @@ use users::{
 #[tokio::main]
 async fn main() -> EmResult<()> {
     log4rs::init_file("users/users_api_server_log.yml", Default::default()).unwrap();
-    let redis_connection_string = std::env::var("REDIS_CONNECTION")?;
-    let redis_session = RedisSessionStore::new(&redis_connection_string)
-        .await
-        .map_err(|err| EmError::Generic(format!("Error: {err}")))?;
     let signing_key = Key::generate();
     api::spawn_api_server::<
         (&str, u16),
         PgConnectionBuilder,
         Postgres,
         PgRoleService,
-        RedisSessionStore,
         PgUserService,
-    >(("127.0.0.1", 8080), db_web_options()?, redis_session, signing_key)
+    >(("127.0.0.1", 8080), db_web_options()?, signing_key)
     .await?;
     Ok(())
 }
