4f3da23adce7d09d1f3142d274ee1886e67d63b4
Steven Thomson <steventhomson9@gmail.com>
2023-05-30 19:10:44 -0400

    feat: move test functionality to common method

--- common/src/database/postgres/test.rs
+++ common/src/database/postgres/test.rs
@@ -1,11 +1,103 @@
+use lazy_regex::{regex, Lazy, Regex};
 use sqlx::PgPool;
 
-use crate::database::{
-    postgres::{format_anonymous_block, Postgres},
-    test::DatabaseTester,
-    Database, RolledBackTransactionResult,
+use crate::{
+    database::{
+        postgres::{format_anonymous_block, Postgres},
+        test::DatabaseTester,
+        Database, RolledBackTransactionResult,
+    },
+    error::EmResult,
+    package_dir, read_file,
 };
 
+static ENUM_REGEX: &Lazy<Regex, fn() -> Regex> = regex!(
+    r"^create\s+type\s+(?P<schema>[^.]+)\.(?P<name>[^.]+)\s+as\s+enum\s*\((?P<labels>[^;]+)\s*\);"
+);
+static COMPOSITE_REGEX: &Lazy<Regex, fn() -> Regex> = regex!(
+    r"^create\s+type\s+(?P<schema>[^.]+)\.(?P<name>[^.]+?)\s+as\s*\((?P<attributes>[^;]+)\);"
+);
+
+/// Check a database build unit to see if it defines an enum creation. If it does, it checks to see
+/// if all it's specified labels can be found within the database's definition of the enum. If the
+/// unit is not an enum, then the function exits immediately with an [Ok].
+pub async fn check_for_enum(pool: &PgPool, file_name: &str) -> EmResult<()> {
+    let file_path = package_dir()?.join("database").join(file_name);
+    let block = read_file(file_path).await?;
+    let Some(captures) = ENUM_REGEX.captures(&block) else {
+        Err("Provided file does not match the ENUM_REGEX pattern")?
+    };
+    let Some(schema) = captures.name("schema") else {
+        Err("No 'schema' capture group present in enum definition")?
+    };
+    let Some(name) = captures.name("name") else {
+        Err("No 'name' capture group present in enum definition")?
+    };
+    let Some(labels) = captures.name("labels") else {
+        Err("No 'labels' capture group present in enum definition")?
+    };
+    let labels: Vec<&str> = labels
+        .as_str()
+        .split(',')
+        .map(|label| label.trim().trim_matches('\''))
+        .collect();
+    sqlx::query("call data_check.check_enum_definition($1,$2,$3)")
+        .bind(schema.as_str())
+        .bind(name.as_str())
+        .bind(labels)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
+/// Check a database build unit to see if it defines a composite creation. If it does, it checks to
+/// see if all it's specified attributes can be found within the database's definition of the
+/// composite. If the unit is not a composite, then the function exits immediately with an [Ok].
+pub async fn check_for_composite(pool: &PgPool, file_name: &str) -> EmResult<()> {
+    let file_path = package_dir()?.join("database").join(file_name);
+    let block = read_file(file_path).await?;
+    let Some(captures) = COMPOSITE_REGEX.captures(&block) else {
+        return Ok(())
+    };
+    let Some(schema) = captures.name("schema") else {
+        Err("No 'schema' capture group present in composite definition")?
+    };
+    let Some(name) = captures.name("name") else {
+        Err("No 'name' capture group present in composite definition")?
+    };
+    let Some(attributes) = captures.name("attributes") else {
+        Err("No 'attributes' capture group present in composite definition")?
+    };
+    let attributes: Vec<&str> = attributes
+        .as_str()
+        .split(',')
+        .map(|label| label.trim())
+        .collect();
+    sqlx::query("call data_check.check_composite_definition($1,$2,$3)")
+        .bind(schema.as_str())
+        .bind(name.as_str())
+        .bind(attributes)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
+///
+pub async fn run_db_test(pool: &PgPool, test_file: &str) -> EmResult<()> {
+    let tester = PgDatabaseTester::create(pool);
+    let test_path = package_dir()?.join("database/tests").join(test_file);
+    let block = read_file(test_path).await?;
+    let result = tester.execute_anonymous_block_transaction(&block).await;
+    if let Some(block_error) = result.block_error {
+        panic!("{block_error}")
+    }
+    if let Some(transaction_error) = result.block_error {
+        panic!("{transaction_error}")
+    }
+    Ok(())
+}
+
+///
 pub struct PgDatabaseTester {
     pool: PgPool,
 }
--- workflow-engine/src/services/postgres/mod.rs
+++ workflow-engine/src/services/postgres/mod.rs
@@ -10,13 +10,10 @@ pub(crate) mod test {
     use common::{
         database::{
             connection::ConnectionBuilder,
-            postgres::{connection::PgConnectionBuilder, test::PgDatabaseTester},
-            test::DatabaseTester,
+            postgres::{connection::PgConnectionBuilder},
         },
         error::EmResult,
-        package_dir, read_file,
     };
-    use lazy_regex::{regex, Lazy, Regex};
     use rstest::{fixture, rstest};
     use sqlx::PgPool;
 
@@ -36,29 +33,9 @@ pub(crate) mod test {
     #[case::workflow_tasks("task/workflow_tasks.pgsql")]
     #[tokio::test]
     async fn database_test(database: PgPool, #[case] test_file: &str) -> EmResult<()> {
-        let tester = PgDatabaseTester::create(&database);
-        let test_path = package_dir()?.join("database/tests").join(test_file);
-        let block = read_file(test_path).await?;
-        let result = tester.execute_anonymous_block_transaction(&block).await;
-        if let Some(block_error) = result.block_error {
-            panic!("{block_error}")
-        }
-        if let Some(transaction_error) = result.block_error {
-            panic!("{transaction_error}")
-        }
-        Ok(())
+        common::database::postgres::test::run_db_test(&database, test_file).await
     }
 
-    static ENUM_REGEX: &Lazy<Regex, fn() -> Regex> = regex!(
-        r"^create\s+type\s+(?P<schema>[^.]+)\.(?P<name>[^.]+)\s+as\s+enum\s*\((?P<labels>[^;]+)\s*\);"
-    );
-    static COMPOSITE_REGEX: &Lazy<Regex, fn() -> Regex> = regex!(
-        r"^create\s+type\s+(?P<schema>[^.]+)\.(?P<name>[^.]+?)\s+as\s*\((?P<attributes>[^;]+)\);"
-    );
-
-    /// Check a database build unit to see if it defines an enum creation. If it does, it checks to
-    /// see if all it's specified labels can be found within the database's definition of the
-    /// enum. If the unit is not an enum, then the function exits immediately with an [Ok].
     #[rstest]
     #[case::executor_status("executor/executor_status.pgsql")]
     #[case::job_type("job/job_type.pgsql")]
@@ -66,38 +43,9 @@ pub(crate) mod test {
     #[case::workflow_run_status("workflow/workflow_run_status.pgsql")]
     #[tokio::test]
     async fn check_for_enum(database: PgPool, #[case] file_name: &str) -> EmResult<()> {
-        let file_path = package_dir()?.join("database").join(file_name);
-        let block = read_file(file_path).await?;
-        let Some(captures) = ENUM_REGEX.captures(&block) else {
-            Err("Provided file does not match the ENUM_REGEX pattern")?
-        };
-        let Some(schema) = captures.name("schema") else {
-            Err("No 'schema' capture group present in enum definition")?
-        };
-        let Some(name) = captures.name("name") else {
-            Err("No 'name' capture group present in enum definition")?
-        };
-        let Some(labels) = captures.name("labels") else {
-            Err("No 'labels' capture group present in enum definition")?
-        };
-        let labels: Vec<&str> = labels
-            .as_str()
-            .split(',')
-            .map(|label| label.trim().trim_matches('\''))
-            .collect();
-        sqlx::query("call data_check.check_enum_definition($1,$2,$3)")
-            .bind(schema.as_str())
-            .bind(name.as_str())
-            .bind(labels)
-            .execute(&database)
-            .await?;
-        Ok(())
+        common::database::postgres::test::check_for_enum(&database, file_name).await
     }
 
-    /// Check a database build unit to see if it defines a composite creation. If it does, it checks
-    /// to see if all it's specified attributes can be found within the database's definition of
-    /// the composite. If the unit is not a composite, then the function exits immediately with
-    /// an [Ok].
     #[rstest]
     #[case("job/schedule_entry.pgsql")]
     #[case("task/task_rule.pgsql")]
@@ -106,31 +54,6 @@ pub(crate) mod test {
     #[case("task/workflow_task_request.pgsql")]
     #[tokio::test]
     async fn check_for_composite(database: PgPool, #[case] file_name: &str) -> EmResult<()> {
-        let file_path = package_dir()?.join("database").join(file_name);
-        let block = read_file(file_path).await?;
-        let Some(captures) = COMPOSITE_REGEX.captures(&block) else {
-            return Ok(())
-        };
-        let Some(schema) = captures.name("schema") else {
-            Err("No 'schema' capture group present in composite definition")?
-        };
-        let Some(name) = captures.name("name") else {
-            Err("No 'name' capture group present in composite definition")?
-        };
-        let Some(attributes) = captures.name("attributes") else {
-            Err("No 'attributes' capture group present in composite definition")?
-        };
-        let attributes: Vec<&str> = attributes
-            .as_str()
-            .split(',')
-            .map(|label| label.trim())
-            .collect();
-        sqlx::query("call data_check.check_composite_definition($1,$2,$3)")
-            .bind(schema.as_str())
-            .bind(name.as_str())
-            .bind(attributes)
-            .execute(&database)
-            .await?;
-        Ok(())
+        common::database::postgres::test::check_for_composite(&database, file_name).await
     }
 }
