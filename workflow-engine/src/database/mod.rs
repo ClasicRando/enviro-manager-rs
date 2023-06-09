46a1eb4d21cdad937efacd78c51eb450d2e73aa2
Steven Thomson <steventhomson9@gmail.com>
2023-06-08 22:18:58 -0400

    feat: restore database test code

--- workflow-engine/src/database/mod.rs
+++ workflow-engine/src/database/mod.rs
@@ -23,3 +23,51 @@ pub fn db_options() -> EmResult<PgConnectOptions> {
         .password(&env::var("WE_PASSWORD")?);
     Ok(options)
 }
+
+#[cfg(test)]
+#[allow(clippy::expect_used)]
+pub(crate) mod test {
+    use common::{
+        database::{connection::ConnectionBuilder, postgres::connection::PgConnectionBuilder},
+        error::EmResult,
+    };
+    use rstest::{fixture, rstest};
+    use sqlx::PgPool;
+
+    use crate::database::db_options;
+
+    #[fixture]
+    pub(crate) fn database() -> PgPool {
+        let options = db_options().expect("Failed to create test database options");
+        PgConnectionBuilder::create_pool_lazy(options, 1, 1)
+    }
+
+    #[rstest]
+    #[case::clean_executors("executor/clean_executors.pgsql")]
+    #[case::next_run_job_schedule("job/next_run_job_schedule.pgsql")]
+    #[tokio::test]
+    async fn database_test(database: PgPool, #[case] test_file: &str) -> EmResult<()> {
+        common::database::postgres::test::run_db_test(&database, test_file).await
+    }
+
+    #[rstest]
+    #[case::executor_status("executor/executor_status.pgsql")]
+    #[case::job_type("job/job_type.pgsql")]
+    #[case::task_status("workflow_run/task_status.pgsql")]
+    #[case::workflow_run_status("workflow_run/workflow_run_status.pgsql")]
+    #[tokio::test]
+    async fn check_for_enum(database: PgPool, #[case] file_name: &str) -> EmResult<()> {
+        common::database::postgres::test::check_for_enum(&database, file_name).await
+    }
+
+    #[rstest]
+    #[case("job/schedule_entry.pgsql")]
+    #[case("workflow_run/task_rule.pgsql")]
+    #[case("workflow_run/workflow_run_task.pgsql")]
+    #[case("workflow/workflow_task.pgsql")]
+    #[case("workflow/workflow_task_request.pgsql")]
+    #[tokio::test]
+    async fn check_for_composite(database: PgPool, #[case] file_name: &str) -> EmResult<()> {
+        common::database::postgres::test::check_for_composite(&database, file_name).await
+    }
+}
