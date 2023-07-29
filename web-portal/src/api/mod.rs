9e12230f0ddd3c967deb41e5491f56aeef5b69dd
Steven Thomson <steventhomson9@gmail.com>
2023-07-28 21:58:41 -0400

    chore: remove unused struct

--- web-portal/src/api/mod.rs
+++ web-portal/src/api/mod.rs
@@ -3,12 +3,6 @@ pub mod users;
 pub mod workflow_engine;
 
 use actix_web::web;
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-struct ModalIdQuery {
-    id: String,
-}
 
 pub fn service() -> actix_web::Scope {
     web::scope("/api")
